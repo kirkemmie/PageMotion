82f16d847c4587bb01004426e40fb8686c8f9cae
kirkemmie <guxtavopaes@hotmail.com>
2013-09-20 17:24:44 -0300

    Finalmente

--- js/PageMotion.js
+++ js/PageMotion.js
@@ -1,56 +1,67 @@
-(function($){
-	$.fn.extend({ 
-		pageMotion: function(opcoes) {
-
-			//Valores padrões
-			var defaults = {
-				seila: 60,
-				naosei: 10
-			};
-			
-			var options = $.extend(defaults, options);
+function PageMotion(){
+	var QPag;
+	var Title = new Array();
+	
+	$('.PageMotion').each(function(i){
 		
-    		return $('.PageMotion').each(function(i){
+		QPag = i;
+		
+		$(this).attr('id', 'PageMotion'+i);
+		Title[i] = $(this).attr('title');
+		
+	});
+
+	if(QPag > 0){
+		
+		if(PageMotionProperties.bottom == true){
+			$('body').append('<div class="PageMotion-Menu PageMotion-bottom"></div>');
+		}else{
+			$('body').append('<div class="PageMotion-Menu PageMotion-top"></div>');
+		}
+		
+		
+		
+		for(j=0;j<=QPag;j++){
 			
-				QPag = i;
-				
-				$(this).attr('id', 'PageMotion'+i);
-				Title[i] = $(this).attr('title');
-				
-			});
-
-			if(QPag > 0){
-				$('body').append('<div class="PageMotion-Menu"></div>');
-				
-				for(j=0;j<=QPag;j++){
-					
-					if(j == 0){
-						$('.PageMotion-Menu').append('<div class="PageMotion-Item Hover" id="PageMotion-Item'+j+'">'+Title[j]+'</div>');
-						$('#PageMotion'+j).show();
-					}else{
-						$('.PageMotion-Menu').append('<div class="PageMotion-Item" id="PageMotion-Item'+j+'">'+Title[j]+'</div>');
-					}
-					
-				}
-				
-				var P = 100/(QPag+1);
-
-				$('.PageMotion-Item').css('width', P+'%');
-				
+			if(j == 0){
+				$('.PageMotion-Menu').append('<div class="PageMotion-Item Hover" id="PageMotion-Item'+j+'">'+Title[j]+'</div>');
+				$('#PageMotion'+j).show();
+			}else{
+				$('.PageMotion-Menu').append('<div class="PageMotion-Item" id="PageMotion-Item'+j+'">'+Title[j]+'</div>');
 			}
 			
-			$('.PageMotion-Item').each(function(q){
-				
-				$('#PageMotion-Item'+q).click(function(){
-					
-					$('.PageMotion-Item').removeClass('Hover');
-					$(this).addClass('Hover');
-					
-					$('.PageMotion').hide();
-					$('#PageMotion'+q).fadeIn();
-					
-				});				
-			});
-    	}
+		}
+		
+		var P = 100/(QPag+1);
+
+		$('.PageMotion-Item').css('width', P+'%');
+		
+	}
+	
+	$('.PageMotion-Item').each(function(q){
+		
+		$('#PageMotion-Item'+q).click(function(){
+			
+			$('.PageMotion-Item').removeClass('Hover');
+			$(this).addClass('Hover');
+			
+			$('.PageMotion').hide();
+			$('#PageMotion'+q).fadeIn(PageMotionProperties.speed);
+			
+		});
+		
 	});
-})(jQuery);
+}
+
+$(document).ready(function(){
+	
+	PageMotionProperties = {
+	
+		bottom:true,
+		speed:5000
+		
+	}
+	
+	PageMotion();
+	
+});
